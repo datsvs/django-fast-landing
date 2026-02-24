76165181f65a651c7dee94b9e64e47866fefda0a
Vadim Dats <datsvs@gmail.com>
2026-02-24 19:09:21 +0200

    fix menu

--- core/templatetags/menu_tags.py
+++ core/templatetags/menu_tags.py
@@ -1,16 +1,22 @@
 from django import template
 # Предположим, у вас есть модель меню
-from core.models import MenuItem
+from core.models import Menu
 
 register = template.Library()
 
 @register.inclusion_tag('menu/menu-loader.html', takes_context=True)
 def show_menu(context, slug, template_name='menu-default'):
-    # Получаем пункты меню из базы
-    items = MenuItem.objects.filter(menu__slug=slug).order_by('order')
+    try:
+        # Получаем объект меню целиком вместе с элементами
+        menu = Menu.objects.prefetch_related('items').get(slug=slug)
+        items = menu.items.all().order_by('order')
+    except Menu.DoesNotExist:
+        menu = None
+        items = []
     # Формируем путь к конкретному файлу верстки
     full_template_path = f'menu/{template_name}.html'
     return {
+        'menu': menu,
         'menu_items': items,
         'template_name': full_template_path,
         # Чтобы работали активные ссылки
